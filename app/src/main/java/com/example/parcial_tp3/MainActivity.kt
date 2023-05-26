ab8c14f83bd8da7d7e3de883e8778fb0cbe80bd4
JuanFerreyros <juansferreyros@gmail.com>
2023-05-26 16:49:39 -0300

    commit inicial(incompleto)

--- app/src/main/java/com/example/parcial_tp3/MainActivity.kt
+++ app/src/main/java/com/example/parcial_tp3/MainActivity.kt
@@ -2,23 +2,73 @@ package com.example.parcial_tp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.example.parcial_tp3.Fragments.FavouritesFragment
+import com.example.parcial_tp3.Fragments.ProfileFragment
+import com.example.parcial_tp3.Fragments.SettingsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomNavView : BottomNavigationView
-    private lateinit var navHostFragment : NavHostFragment
+    private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
         bottomNavView = findViewById(R.id.bottom_bar)
 
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
 
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.navigationView)
+
+        toggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
+            when (it.itemId) {
+                R.id.nav_profile -> replaceFragment(ProfileFragment(), it.title.toString())
+                R.id.nav_settings -> replaceFragment(SettingsFragment(), it.title.toString())
+                R.id.nav_favourites -> replaceFragment(FavouritesFragment(), it.title.toString())
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment, title: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
