66ebcb1410232e10e4dc353039b45ab7af736922
PseudoEmpirico <facundopradells@gmail.com>
2023-05-26 16:58:15 -0300

    Correccion

--- app/src/main/java/com/example/parcial_tp3/Adapters/CarListAdapter.kt
+++ app/src/main/java/com/example/parcial_tp3/Adapters/CarListAdapter.kt
@@ -1,16 +1,21 @@
 package com.example.parcial_tp3.Adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parcial_tp3.Entities.Car
-import com.example.parcial_tp3.Holders.CarViewHolder
 import com.example.parcial_tp3.R
 
 
 class CarListAdapter(
     private val carList: MutableList<Car>
-) : RecyclerView.Adapter<CarViewHolder>() {
+) : RecyclerView.Adapter<CarListAdapter.CarViewHolder>() {
+
+    class CarViewHolder(itemView : View): RecyclerView.ViewHolder(itemView) {
+        val textName: TextView = itemView.findViewById(R.id.textView_car)
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.car_item_view, parent, false)
         return CarViewHolder(view)
